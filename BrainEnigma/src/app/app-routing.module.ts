95fd272bf1339c85dbaab8de97328d7ab7e1dabe
UgoBit <ugo.bittoni@gmail.com>
2023-05-26 15:04:12 +0200

    Mise en place du front v0.2

--- BrainEnigma/src/app/app-routing.module.ts
+++ BrainEnigma/src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ChooseComponent } from './choose/choose.component';
 import { ChooseDifficultyComponent } from './choose-difficulty/choose-difficulty.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path:'', component: HomepageComponent},
+  {path:'signUp', component: SignUpComponent},
+  {path:'logIn', component: LogInComponent},
   {path:'profile', component: ProfileComponent},
   {path:'choose', component: ChooseComponent},
-  {path:'difficulty', component: ChooseDifficultyComponent}
+  {path:'difficulty', component: ChooseDifficultyComponent},
+  {path:'contact', component: ContactComponent},
+  {path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
